ac85967884d71f2cb582347cc849e7a9fcbac17e
zaidmulla007 <zaidmulla1122@gmail.com>
2024-10-11 11:43:42 +0530

    carts all codes with postman

--- database/migrations/2024_10_11_060407_create_carts_table.php
+++ database/migrations/2024_10_11_060407_create_carts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cart', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->increments('id'); // Auto-incrementing ID
             
             $table->unsignedInteger('user_id'); // User ID as unsigned integer
