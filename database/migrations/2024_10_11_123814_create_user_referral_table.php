06892bf4035b5ddf08a00b2abd585ac4f6b6c40c
zaidmulla007 <zaidmulla1122@gmail.com>
2024-10-11 18:12:07 +0530

    created user_referral

---
+++ database/migrations/2024_10_11_123814_create_user_referral_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_referral', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('reg_user_id'); // Add reg_user_id as an unsigned big integer
+            $table->unsignedBigInteger('referral_id'); // Add referral_id as an unsigned big integer
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_referral');
+    }
+};
