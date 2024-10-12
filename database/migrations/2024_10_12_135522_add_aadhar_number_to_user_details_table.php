7a034aac4033b2c9c638249e3d6a8c71792e6635
zaidmulla007 <zaidmulla1122@gmail.com>
2024-10-12 19:27:06 +0530

    updated product and user details with priority and aadhar_number

---
+++ database/migrations/2024_10_12_135522_add_aadhar_number_to_user_details_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->integer('aadhar_number')->nullable()->after('user_id'); // Add aadhar_number after user_id
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->dropColumn('aadhar_number'); // Drop aadhar_number column
+        });
+    }
+};
