689046d765b6d921731d34079af0f3586f041b04
zaidmulla007 <zaidmulla1122@gmail.com>
2024-10-15 16:37:49 +0530

    updated user_id with earning

---
+++ database/migrations/2024_10_15_110402_drop_user_id_from_earnings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('earnings', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('earnings', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable(); // Adjust as necessary
+            //
+        });
+    }
+};
---
+++ database/migrations/2024_10_15_110530_add_user_id_to_earnings_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('earnings', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->after('second_referral_purchase_total')->nullable(); // Adjust nullable as needed
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('earnings', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
