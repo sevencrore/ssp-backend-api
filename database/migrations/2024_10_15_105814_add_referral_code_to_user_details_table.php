308b331f5a2da4cb9e9ebf5c6c1c2467f42aba75
zaidmulla007 <zaidmulla1122@gmail.com>
2024-10-15 16:32:53 +0530

    updated referral_code in user details

---
+++ database/migrations/2024_10_15_105814_add_referral_code_to_user_details_table.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->string('referral_code')->after('aadhar_number')->nullable(); // Adjust nullable as needed
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->dropColumn('referral_code');
+        });
+    }
+};
