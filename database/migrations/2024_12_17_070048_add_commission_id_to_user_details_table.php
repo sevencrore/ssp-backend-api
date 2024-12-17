0c31edfae81966b2d673f0a228554f06043e281b
Shrishail20 <136682949+Shrishail20@users.noreply.github.com>
2024-12-17 12:51:50 +0530

    add the foreign keyy for the user_details table comission_id

---
+++ database/migrations/2024_12_17_070048_add_commission_id_to_user_details_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCommissionIdToUserDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            
+                $table->dropColumn('commission');
+                $table->dropColumn('minimum_order');
+            
+
+            // Add 'commission_id' as a foreign key referencing 'id' on 'commissions' table
+            $table->unsignedBigInteger('commission_id')->nullable()->after('user_id');
+            $table->foreign('commission_id')->references('id')->on('commissions')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_details', function (Blueprint $table) {
+            // Drop the foreign key and 'commission_id' column
+                $table->dropForeign(['commission_id']);
+                $table->dropColumn('commission_id');
+
+            // Re-add 'commission' and 'minimum_order' columns
+            $table->string('commission')->nullable();
+            $table->integer('minimum_order')->nullable();
+        });
+    }
+}
