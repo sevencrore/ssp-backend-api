849f916b2104cf8a9c14b8458ef1f296d7aebbb3
zaidmulla007 <zaidmulla1122@gmail.com>
2025-04-24 18:08:43 +0530

    updated vendor transaction

--- app/Http/Controllers/API/VendorTransactionsController.php
+++ app/Http/Controllers/API/VendorTransactionsController.php
@@ -17,7 +17,7 @@ class VendorTransactionsController extends Controller
                 'total_amount' => 'required|numeric',
                 'admin_commission_amount' => 'required|numeric',
                 'tds_charges_amount' => 'required|numeric',
-                'amount_paid' => 'required|integer',
+                'amount_paid' => 'required|numeric',
                 'transaction_id' => 'required|string|unique:vendor_transactions,transaction_id',
                 'status' => 'required|integer', // 1 for unpaid, 2 for paid, 3 refunded, 4 pending
                 'attachment' => 'nullable|file|mimes:jpeg,jpg,png,gif,pdf|max:20480',
