8e07946e0342438e007242db60377925c7b1762d
jyotisannapanavar <136812071+jyotisannapanavar@users.noreply.github.com>
2025-04-26 11:56:27 +0530

    minor changes adede

--- app/Http/Controllers/ComissionController.php
+++ app/Http/Controllers/ComissionController.php
@@ -37,7 +37,7 @@ public function getAllPaginated(Request $request)
         $perPage = $request->get('per_page', 10); // Default 10 per page
 
         // Optional: search logic (if needed in future)
-        $query = Comission::orderBy('created_at', 'desc');
+        $query = Comission::orderBy('created_at');
 
         $queryParams = Arr::except($request->query(), []);
 
