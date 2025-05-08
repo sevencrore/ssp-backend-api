8483f2876897c65843cb8fb217832bf0bceba5a0
Shrishail20 <136682949+Shrishail20@users.noreply.github.com>
2025-05-08 13:09:29 +0530

    added the aminproduct transactions excel export

--- routes/api.php
+++ routes/api.php
@@ -233,6 +233,7 @@ Route::middleware('auth:sanctum')->group(function () {
       Route::put('/admin/update-user-address', [AdminController::class, 'updateUserAddressByAdmin']);
       Route::put('/admin/update-user-state/{id}', [AdminController::class, 'setStatus']);
       Route::get('admin/Ordertransactions', [UserPaymentController::class, 'getAdminproduct_transactions'])->name('userPayment.getAdminproduct_transactions');
+      Route::get('admin/excel/Ordertransactions', [ExcelExportController::class, 'exportAdminProductTransactions'])->name('excel.exportAdminProductTransactions');
       Route::get('admin/getUserDetailsByEmail', [UserDetailsController::class, 'getUserDetailsByEmail']);
       Route::get('admin/getUserAddressByEmail', [AddressController::class, 'getUserAddressByEmail']);
       Route::post('/admin/updateVendor', [CustomerVendorController::class, 'updateVendor']); //update the customer vendor mapping
